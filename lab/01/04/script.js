31665dbf59f3e614c5d9e39b06ddd0decbde6006
Turstle <turstle17@gmail.com>
2020-04-06 17:45:36 +0200

    Training : done.

---
+++ lab/01/04/script.js
@@ -0,0 +1,19 @@
+
+const nom = 'Aymeric';
+
+function displayBooleanTrue() {
+    const bool = true;
+    console.log(bool);
+}
+
+function displayName() {
+    console.log(nom);
+}
+
+function displayNameUppercase() {
+    console.log(nom.toUpperCase());
+}
+
+function displaySum() {
+    console.log(10 + 10.25);
+}
